d18d5cf635c64d536aec184c156fb33965a7248f
IV4 <106611702+ValeryIV4@users.noreply.github.com>
2023-04-18 12:33:30 +0300

    Add files via upload

---
+++ 6_Inis/input.js
@@ -0,0 +1,56 @@
+var draggable = document.getElementById('draggable');
+var initialX, initialY;
+var currentX, currentY;
+var xOffset = 0, yOffset = 0;
+var isDragging = false;
+
+draggable.addEventListener('touchstart', dragStart);
+draggable.addEventListener('touchmove', drag);
+draggable.addEventListener('touchend', dragEnd);
+document.addEventListener('dblclick', handleDoubleClick);
+
+function dragStart(e) {
+  initialX = e.touches[0].clientX - xOffset;
+  initialY = e.touches[0].clientY - yOffset;
+
+  if (e.target === draggable) {
+    isDragging = true;
+  }
+}
+
+function drag(e) {
+  if (isDragging) {
+    e.preventDefault();
+
+    currentX = e.touches[0].clientX - initialX;
+    currentY = e.touches[0].clientY - initialY;
+
+    xOffset = currentX;
+    yOffset = currentY;
+
+    setTranslate(currentX, currentY, draggable);
+  }
+}
+
+function dragEnd(e) {
+  initialX = currentX;
+  initialY = currentY;
+
+  isDragging = false;
+}
+
+function setTranslate(xPos, yPos, el) {
+  el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
+}
+
+function handleDoubleClick(event) {
+if (event.target.classList.contains('target')) {
+selectedElement = event.target;
+selectedElement.classList.add('dragging');
+startPosition.x = event.clientX;
+startPosition.y = event.clientY;
+
+document.addEventListener('mousemove', handleDrag);
+document.addEventListener('mouseup', handleDrop);
+}
+}
